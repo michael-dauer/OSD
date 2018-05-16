5ad664c904c4f8c7721fb38579e55430c379c8d5
Marlene Glawischnig <marlene.glawischnig@edu.fh-joanneum.at>
2018-05-16 15:41:45 +0200

    JSON File hinzugefügt.

--- Reaction_Game/Reaction_Game/main.cpp
+++ Reaction_Game/Reaction_Game/main.cpp
@@ -7,9 +7,27 @@
 #include "Reaction_game.h"
 #include "piproxy.h"
 #include "pin_config.h"
+#include "json.hpp"
+#include <fstream>
+#include <iostream>
+
+// for convenience
+using json = nlohmann::json;
 
 int main(void)
-{	
+{
+	// read a JSON file9
+	std::ifstream i("..\\Reaction_Game\\pins.json");
+	json pin_config;
+	i >> pin_config;
+
+	for (json::iterator it = pin_config.begin(); it != pin_config.end(); ++it) {
+		std::cout << *it << '\n';
+	}
+
+	std::cout << pin_config["p1_led"] << std::endl;
+
+
 	wiringPiSetup();
 
 	pinMode(PLAYER1_BUTTON, INPUT);
